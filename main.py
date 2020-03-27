fc1a579de022be50cf33d31c43f784c0d126cbaa
Nikita Denisov <deNIK88@yandex.ru>
2020-03-27 21:09:55 +0300

    Сделал линию (пушку) фиксированной длины

--- main.py
+++ main.py
@@ -1,24 +1,45 @@
-from random import randrange as rnd, choice
 import tkinter as tk
-import time
-
+import math
 
 root = tk.Tk()
 root.geometry('800x600')
 canv = tk.Canvas(root)
-canv.pack(fill=tk.BOTH, expand=1)
+canv.pack(fill="both", expand=1)
 
 
-class gun():
+class Gun():
     def __init__(self):
-        self.id = canv.create_line(40, 500, 80, 500, width = 7)
+        self.coordX = 40
+        self.coordY = 500
+        self.id = canv.create_line(self.coordX, self.coordY, 80, 500, width = 7)
+
     def targetting(self, event):
-        canv.coords(self.id, 40, 500, event.x, event.y)
+        """Прицеливание. Зависит от положения мыши."""
+
+        # Шаг 1. Находим длину вектора (линия от x, y до курсора мыши).
+        mouseX = event.x
+        mouseY = event.y
+        dlina = math.sqrt((mouseX - self.coordX)**2 + (mouseY - self.coordY)**2)
+        # По формуле корень квадратный из суммы квадратов разностей координат
+
+        # Шаг 2. Привожу длину вектора к желаемой.
+        """ Нормализация вектора — это преобразование заданного вектора в вектор в том же направлении,
+        но с единичной длиной. Для нормализации вектора нужно каждую компоненту поделить на длину вектора. """
+        companente1 = (mouseX - self.coordX) / dlina
+        compenente2 = (mouseY - self.coordY) / dlina
+        # Получится нормализованный вектор длиной в единицу √companente1**2 + compenente2**2
+
+        # Шаг 3. Рисую вектор новой длины (50)
+        newCoordX = self.coordX + (companente1 * 50)
+        newCoordY = self.coordY + (compenente2 * 50)
+        canv.coords(self.id, self.coordX, self.coordY, newCoordX, newCoordY)
+
+    def tension(self):  # tension - натяжение
+        all()
 
 
 
-g1 = gun()
+
+g1 = Gun()
 root.bind('<Motion>', g1.targetting)
-
-
 tk.mainloop()
